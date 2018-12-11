6e9eeb237a67b40e1dc2c0e133ccee6d381fd358
Stijn De Pestel <stijn.depestel@gmail.com>
2018-12-11 09:43:39 +0100

    Added some test for number validation using the objectvalidator.

--- packages/motil-validation-engine/src/__tests__/ObjectValidator.test.ts
+++ packages/motil-validation-engine/src/__tests__/ObjectValidator.test.ts
@@ -1,23 +1,78 @@
-import { ObjectValidator, ObjectValidationResult } from "..";
-
 import "ts-jest";
+import {ObjectValidator} from "..";
 
-function generateTestObject () {
-    return {
-        testString: "some_text",
-        randomInt: Math.round(Math.random() * 1000),
-        fixedInt: 5,
-        emptyString: ""
-    }
-}
+describe('ObjectValidator', () => {
 
-it('testString is required and should be valid', () => {
-    let validationEngine = new ObjectValidator(generateTestObject())
-        .field("fixedInt")
-            .numberMax(10)
-        .and("fixedInt")
-            .numberMax(3)
-        .validate();
- 
-    console.log(JSON.stringify(validationEngine));
+    describe('#numberValidation', () => {
+
+        it('should validate a number is less than some number', () => {
+            const toValidate = {
+                number: 2,
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeTruthy();
+        });
+
+        it('should validate a number is not less than some number', () => {
+            const toValidate = {
+                number: 4,
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeFalsy();
+        });
+
+        it('should validate to true if the number is equal to the maximum allowed value', () => {
+            const toValidate = {
+                number: 3,
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeTruthy();
+        });
+
+        it('should validate negative numbers if no lower bounds has been configured', () => {
+            const toValidate = {
+                number: -1,
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeTruthy();
+        });
+
+        it('should not validate if the number is not a number', () => {
+            const toValidate = {
+                number: 'test',
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeFalsy();
+        });
+
+        it('should validate if the number is undefined while it is not required', () => {
+            const toValidate = {
+                number: undefined,
+            };
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeTruthy();
+        });
+
+        it('should validate if the number is missing while it is not required', () => {
+            const toValidate = {};
+            const validationResult = new ObjectValidator(toValidate)
+                .field('number').numberMax(3).validate();
+
+            expect(validationResult.isValid).toBeTruthy();
+        });
+
+    });
+
 });
