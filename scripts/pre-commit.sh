31714d65f38f0f3da30bd140f4c3b3aceb49a24b
Stijn De Pestel <stijn.depestel@gmail.com>
2018-12-17 16:05:25 +0100

    Added readmes, updated contributer & license information. Renamed motil-core to motil-components.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FILES=`git diff --cached --name-only | grep -E "^[^_]*\.ts[x]?$"`;
+FILES=`git diff --cached --name-only | grep -E "^.*\/src\/[^_]*\.ts[x]?$"`;
 
 if [ "$FILES" ];
 then
